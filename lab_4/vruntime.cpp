769936b4edf60b3abc15bf8ef132d469ac5689ea
Taylor <lxmachina@gmail.com>
2016-03-20 18:17:02 -0400

    Doc update

--- lab_4/vruntime.cpp
+++ lab_4/vruntime.cpp
@@ -7,6 +7,7 @@
 
 // TODO Parameterize the variance
 const double Process::LIFE_VARIANCE = 40.0;
+// TODO Move lifetime bounds to another place
 const unsigned Process::LIFE_MIN = 10u;
 unsigned Process::_next_id = 0;
 
