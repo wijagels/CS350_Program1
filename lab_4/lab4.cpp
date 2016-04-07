f7eb4e821ef046a9c785b5f779d0d9c3973fa77d
Taylor Foxhall <lxmachina@gmail.com>
2016-04-07 12:49:33 -0400

    stuff

--- lab_4/lab4.cpp
+++ lab_4/lab4.cpp
@@ -5,11 +5,9 @@
 #include <fstream>
 #include <string>
 
-void check(bool p, const char *message) {
-  if (p) {
-    loge(message);
-    exit(1);
-  }
+void print_usage() {
+	std::cerr << "USAGE: ./lab4 -t [address space size] -n [page size] -l [process lifetime average] "
+		"-p [locality probability] -o [output file]\n";
 }
 
 int main(int argc, char **argv) {
@@ -22,24 +20,35 @@ int main(int argc, char **argv) {
   std::ofstream test_file;
 
 
-  while ((flag = getopt(argc, argv, "ut:n:l:p:o:")) != -1) {
+  while ((flag = getopt(argc, argv, "ut:n:l:p:o:w:")) != -1) {
     switch (flag) {
     case 'n':
       // Page size
       n = std::stoul(optarg, NULL, 10);
-      check(sandwich(PAGE_MIN, n, PAGE_MAX), "Invalid page size: 2^%u");
+      if (sandwich(PAGE_MIN, n, PAGE_MAX)) {
+				loge("Invalid page size: 2^%u", n);
+				exit(1);
+			}
       break;
     case 't':
       // Address space size
       t = std::stoul(optarg, NULL, 10);
-      check(sandwich(ADDR_MIN, t, ADDR_MAX), "Invalid address space size");
+			if (sandwich(ADDR_MIN, t, ADDR_MAX)) {
+				loge("Invalid address space size: 2^%u", t);
+				exit(1);
+			}
       break;
     case 'p':
+			// Locality control
       p = std::stod(optarg, NULL);
+			if (0.0 < p && p < 1.0) {
+				loge("Locality probability not valid: %f. Must be between 0 and 1", p);
+				exit(1);
+			}
       break;
     case 'l':
-      l = std::stoi(optarg, NULL, 10);
-      check(0.0 < l && l < 1.0, "Locality probability not valid: Must be between 0 and 1");
+			// Lifetime average
+			l = std::stoi(optarg, NULL, 10);
       break;
     case 'o':
       test_file.open(optarg);
@@ -47,14 +56,17 @@ int main(int argc, char **argv) {
     case 'u':
     case '?':
     default:
-      // Just kick out and let main print usage
-      std::cerr << "USAGE: ./lab4 -t [address space size] -n [page size] -l [process lifetime average] "
-              "-p [locality probability]\n";
+			// Just kick out and let main print usage
+			print_usage();
       exit(0);
     }
   }
 
-  check(n < t, "Address space not divisible by address size");
+	check(n < t,);
+	if (n < t) {
+		loge("Address space not divisible by address size");
+		exit(1);
+	}
 
   VRuntime vruntime{10, t, n, p, l};
   // Output input file here
