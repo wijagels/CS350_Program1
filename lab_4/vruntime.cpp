c0e1b337dcf3ac6fcaa07f5a3e89aca08f55c307
Taylor Foxhall <lxmachina@gmail.com>
2016-04-09 22:42:01 -0400

    Added memory phases

--- lab_4/vruntime.cpp
+++ lab_4/vruntime.cpp
@@ -2,6 +2,7 @@
 #include "debug.h"
 #include <random>
 #include <algorithm>
+#include <utility>
 
 // Process impl
 
@@ -11,10 +12,53 @@ const double Process::LIFE_VARIANCE = 40.0;
 const int Process::LIFE_MIN = 10u;
 unsigned Process::_next_id = 0;
 
-void Process::access(unsigned long long addr) {
+bool Process::access(unsigned long long addr) {
   assert(is_alive());
   _age++;
-  _last_addr = addr;
+	_refs->push_back(addr / _page_size);
+	return is_alive();
+}
+
+int Process::run() {
+	unsigned page_size = (1 << _page_size);
+	unsigned last_page = this->last_addr() / page_size;
+	unsigned long long next_ref;
+	unsigned long long lower = last_page * page_size, upper = lower + page_size;
+	int old_history_sz = _refs->size();
+	std::default_random_engine gen;
+	std::bernoulli_distribution local_dist(_locality);
+
+	// MAGIC
+	for (int i = 0; this->is_alive() && i < 10; i++) {
+		switch (_policy) {
+			case OPEN:
+				if (!local_dist(gen)) {
+					// If local, then generate an address within the same page as the last
+					lower = 0;
+					upper = ~(-1uLL << _addr_size);
+				}
+			case NORMAL:
+				next_ref = rand() % (upper - lower) + lower;
+				this->access(next_ref);
+				break;
+			case HEAVY:
+				next_ref = rand() % (upper - lower) + lower;
+				// MAGIC
+				for (int j = 0; this->access(next_ref+j) &&  j < 10; j++);
+				break;
+			case IO:
+				next_ref = rand() % (upper - lower) + lower;
+				// MAGIC
+				if (i % 10 == 0) {
+					this->access(next_ref);
+				} else {
+					// Get older anyway, but don't access anything
+					_age++;
+				}
+				break;
+		}
+	}
+	return _refs->size() - old_history_sz;
 }
 
 // VRuntime impl
@@ -46,43 +90,32 @@ VRuntime::VRuntime(unsigned procs, unsigned addr_size, unsigned page_size, doubl
 }
 
 std::ostream& operator<<(std::ostream& os, const VRuntime& vr) {
-  std::default_random_engine gen;
+	std::default_random_engine gen;
   std::normal_distribution<double> lifetime_dist(vr.avg_lifetime, Process::LIFE_VARIANCE);
-  std::bernoulli_distribution local_dist(vr.locality);
-  std::vector<Process> procs(vr.max_procs);
+  std::vector<std::unique_ptr<Process> > procs(vr.max_procs);
   // Start the procs
   for (unsigned i = 0; i < vr.max_procs; i++) {
     int life = std::max(static_cast<int>(lifetime_dist(gen)), Process::LIFE_MIN);
-    procs[i] = Process{life};
-    logd("Proc %d: lifetime %u", procs[i].id(), life);
-    os << "START " << procs[i].id() << " " << (1<<(vr.addr_size-vr.page_size)) << "\n";
+		unsigned addr_sz = vr.addr_size, pg_sz = vr.page_size;
+    procs[i] = std::unique_ptr<Process>(new Process{addr_sz, pg_sz, life, vr.locality});
+    logd("Proc %d: lifetime %u", procs[i]->id(), life);
+    os << "START " << procs[i]->id() << " " << (1<<(addr_sz-pg_sz)) << "\n";
   }
 
   int last = procs.size() - 1;
-  int target = 0;
-  unsigned page_size = (1 << vr.page_size);
-  while (last > 0) {
-    if (procs[target].is_alive()) {
-      unsigned long long next_ref, lower = 0, upper = ~(-1uLL << vr.addr_size);
-      // Do a locality test
-      if (local_dist(gen)) {
-        // If local, then generate an address within the same page as the last
-        unsigned last_page = procs[target].last_addr() / page_size;
-        lower = last_page * page_size;
-        upper = lower + page_size;
-      }
-      // Else pick an arbitrary address in the address space
-      next_ref = rand() % (upper - lower) + lower;
-      procs[target].access(next_ref);
-      os << "REFERENCE " << procs[target].id() << " " << next_ref / page_size << "\n";
-
-      // Kill proc if it's time has come
-      if (!procs[target].is_alive()) {
-        // Get someone else to fill the dead proc's spot
-        os << "TERMINATE " << procs[target].id() << "\n";
-        if (target != last) procs[target] = procs[last];
-        last--;
-      }
+	int target = 0; while (last > 0) {
+    if (procs[target]->is_alive()) {
+			int refs_made = procs[target]->run();
+			auto history = procs[target]->history();
+			for (int i = history.size() - refs_made; i < history.size(); i++) {
+				os << "REFERENCE " << procs[target]->id() << " " << history[i] << "\n";
+			}
+			if (!procs[target]->is_alive()) {
+				// Get someone else to fill the dead proc's spot
+				os << "TERMINATE " << procs[target]->id() << "\n";
+				if (target != last) std::swap(procs[target], procs[last]);
+				last--;
+			}
     }
     // TODO Parameterize next process choice, also make less arbitrary
     target = rand() % (last+1);
--- lab_4/vruntime.hpp
+++ lab_4/vruntime.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <memory>
 
 #define PAGE_MIN 5
 #define PAGE_MAX 15
@@ -15,23 +16,36 @@ class Process {
   public:
   static const double LIFE_VARIANCE;
   static const int LIFE_MIN;
-  Process(): _id{_next_id++}, _age{0}, _death_time{0}, _last_addr{0} {}
+	using Policy = enum { NORMAL, OPEN, HEAVY, IO };
+  Process():
+		_id{++_next_id}, _age{0}, _death_time{0}, _addr_size{0}, _page_size{0} {}
   Process(const Process&) = default;
+	Process(Process&&) = default;
   Process& operator=(const Process&) = default;
-  explicit Process(int death): _id{_next_id++}, _age{0}, _death_time{death}, _last_addr{0} {}
+	Process& operator=(Process&&) = default;
+  Process(unsigned addr_sz, unsigned pg_sz, int death, double locality = 0.5):
+		_id{++_next_id}, _age{0}, _death_time{death}, _addr_size{addr_sz}, _page_size{pg_sz},
+		_locality{locality},
+		_refs{std::unique_ptr<std::vector<unsigned long long> >(new std::vector<unsigned long long>)}
+	{}
   ~Process() = default;
   inline bool is_alive() { return _age < _death_time; }
-  void access(unsigned long long);
-  unsigned last_addr() { return _last_addr; }
+  bool access(unsigned long long);
+  unsigned last_addr() { return _refs->back(); }
   inline unsigned id() { return _id; }
   // Method will return a stream of addresses for a phase
-  std::vector<unsigned> run();
+	int run();
+	const std::vector<unsigned long long> history() { return *_refs; }
 
   private:
   unsigned _id;
   int _age;
   int _death_time;
-  unsigned long long _last_addr;
+	unsigned _addr_size;
+	unsigned _page_size;
+	Policy _policy;
+	std::unique_ptr<std::vector<unsigned long long> > _refs;
+	double _locality;
   static unsigned _next_id;
 };
 
@@ -45,8 +59,6 @@ class VRuntime {
   friend std::ostream& operator<<(std::ostream&, const VRuntime&);
 
   private:
-  unsigned addr_size;
-  unsigned page_size;
   double locality;
   int avg_lifetime;
   unsigned max_procs;
